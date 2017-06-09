4a958cd290f5353af500f588ca5a42e16235d0cd
Kirsten <Kknapp@alumni.berklee.edu>
2017-06-09 12:10:42 -0700

    Add border radius changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
+$(document).ready(function() {
+  $("form#vaca-form").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    console.log("1st number: " + number1); // for debugging
+    console.log("2nd number: " + number2); // for debugging
+    console.log("operator: " + operator); // for debugging
+    console.log("result: " + result); // for debugging
+    var result;
+    if (operator === "add") {
+      result  = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
 
+  });
+});
