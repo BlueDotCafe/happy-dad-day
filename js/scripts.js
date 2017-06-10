7ef5570f2218a9371ea1ea920a748a0605d7156a
Kirsten Knapp <kknapp@alumni.berklee.edu>
2017-06-10 13:03:08 -0700

    Add working jquery to display answers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,53 @@
 $(document).ready(function() {
+  $(".suggestion").hide();
   $("form#vaca-form").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
-    var operator = $("input:radio[name=operator]:checked").val();
-    console.log("1st number: " + number1); // for debugging
-    console.log("2nd number: " + number2); // for debugging
-    console.log("operator: " + operator); // for debugging
-    console.log("result: " + result); // for debugging
-    var result;
-    if (operator === "add") {
-      result  = add(number1, number2);
-    } else if (operator === "subtract") {
-      result = subtract(number1, number2);
-    } else if (operator === "multiply") {
-      result = multiply(number1, number2);
-    } else if (operator === "divide") {
-      result = divide(number1, number2);
+/*
+var tally is an object whose properties are named for the destinations and the values of the properties will be the numbers of answers related to that city.
+var answers is an array of strings whose members are the raw selected values from the form: example ["i", "p"]
+*/
+    var tally = {
+      "i":0,"p":0,"c":0
+    };
+    var answers = [
+      $("#eat").find(":selected").val(),
+      $("#vaca").find(":selected").val(),
+      $("#weekend").find(":selected").val(),
+      $("#wear").find(":selected").val(),
+      $("#dinner").find(":selected").val()
+    ];
+
+    for (i in answers) {
+      var currentAnswer = answers[i];
+      console.log(currentAnswer);
+      tally[currentAnswer]++;
     }
-    $("#output").text(result);
+    console.log(tally);
+    /*hide all*/
+    $(".suggestion").hide();
+    if (tally["i"] > tally["p"] && tally["i"] > tally["c"]) {
+      /*show italy*/
+      $(".italy,.italy-ul").show();
+    } else if (tally["p"] > tally["i"] && tally["p"] > tally["c"]) {
+      /*show paris*/
+      $(".paris,.paris-ul").show();
+    } else {
+      /*show cancun*/
+      $(".cancun,.cancun-ul").show();
+    }
+
+
+
+
+
+
 
   });
 });
+/*
+when i submit the form
+i want to tabulate the number of answers correlated with each destination
+example: {italy:3,paris:1,cancun:1}
+for the destination with the most answers, it will reveal the associated div.
+and hide all the other possible answers.
+*/
